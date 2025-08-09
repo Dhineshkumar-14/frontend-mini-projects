6ae208f2840f9965a30745259ac2f6404484d0fc
Dhineshkumar <dhineshkumar.velayudham@gmail.com>
2025-08-09 16:35:59 +0530

    Completed 10-Dad-Joke Mini Project

---
+++ 10-dad-jokes/script.js
@@ -0,0 +1,20 @@
+const jokeEl = document.querySelector(".joke");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", getJoke);
+
+async function getJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  const res = await fetch("https://icanhazdadjoke.com", config);
+
+  const data = await res.json();
+
+  jokeEl.innerText = data.joke;
+}
+
+getJoke();
